602ea163b8ffba116b5f76f6e26aff90ec17fc83
Chris W Jones <chris@christopherjones.us>
2022-06-22 19:57:27 -0400

    Format

--- analyze.py
+++ analyze.py
@@ -6,33 +6,38 @@ def scale_number(number):
     max = 67291
     return int((number - min) / (max - min))
 
+
 # Found here: https://realpython.com/python-histograms/#histograms-in-pure-python
 def ascii_histogram(seq):
-    for k in len(seq):
+    for k in range(len(seq)):
         print("{0:5d} {1}".format(k, "+" * scale_number(seq[k])))
 
 
 def read_csv():
-    data = pd.read_csv('2015 Q1.csv', header=0)
+    data = pd.read_csv("2015 Q1.csv", header=0)
     return data
 
+
 def clean_data(data):
-    cleaned_data = data.astype({
-        'Total duration (ms)': 'int64',
-        'Start date': 'datetime64',
-        'Start station': 'string',
-        'End date': 'datetime64',
-        'End station': 'string',
-        'Bike number': 'string',
-        'Subscription Type': 'string',
-    })
-    cleaned_data = cleaned_data.dropna(subset=['Bike number', 'Subscription Type'])
+    cleaned_data = data.astype(
+        {
+            "Total duration (ms)": "int64",
+            "Start date": "datetime64",
+            "Start station": "string",
+            "End date": "datetime64",
+            "End station": "string",
+            "Bike number": "string",
+            "Subscription Type": "string",
+        }
+    )
+    cleaned_data = cleaned_data.dropna(subset=["Bike number", "Subscription Type"])
     return cleaned_data
+
 
 def analyze_data(data):
     days = [0, 0, 0, 0, 0, 0, 0]
     for (_, row) in data.iterrows():
-        days[row['Start date'].dayofweek] += 1
+        days[row["Start date"].dayofweek] += 1
     ascii_histogram(days)
 
 
